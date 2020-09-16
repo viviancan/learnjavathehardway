7d9e64397e70d584caca29c7f057eb5ee7d89b35
viviancan <vcnales@gmail.com>
2020-09-16 12:12:21 -0500

    completed study drills

--- FirstProg.java
+++ FirstProg.java
@@ -1,6 +1,14 @@
+import java.util.*;
+import java.text.SimpleDateFormat;  
+
+
 public class FirstProg{
 	public static void main(String[] args){
-		System.out.println("I am larning code");
-		System.out.println("Today's date is ");
+		System.out.println("I am learning code");
+		
+		SimpleDateFormat formatter = new SimpleDateFormat("EEEE, MMMM d, y");
+		Date today = new Date();
+
+		System.out.println("Today's date is " + formatter.format(today));
 	}
 }
