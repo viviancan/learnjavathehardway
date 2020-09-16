951fb0c4ecf21488cd019f75e15c8417ea8d5790
viviancan <vcnales@gmail.com>
2020-09-16 14:29:14 -0500

    exercises 2

---
+++ GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+	public static void main(String[] args){
+		System.out.println("+------------------------+");
+		System.out.println("|                        |");
+		System.out.println("|      CORNER STORE      |");
+		System.out.println("|       2020-09-16       |");
+		System.out.println("|                        |");	
+		System.out.println("| Gallons:        10.870 |");	
+		System.out.println("| Price/gallon:  $ 2.089 |");
+		System.out.println("|                        |");
+		System.out.println("| Fuel Total:  	$ 22.71  |");	
+		System.out.println("|                        |");	
+		System.out.println("+------------------------+");
+	}
+
+}
